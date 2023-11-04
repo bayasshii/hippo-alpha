e31217bdd9aaf40039fc1b8bcbd32a8834b7e588
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-11-04 11:45:16 +0900

    useCallback祭

--- frontend/app/src/pages/Edit.tsx
+++ frontend/app/src/pages/Edit.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef } from "react";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Flex } from "../components/Flex";
 import { getAPIData } from "../api/getAPIData";
@@ -64,48 +64,54 @@ export const Edit = () => {
     fetchData();
   }, [simulation_id]);
 
-  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (simulation == null) return;
-    setSimulation({ ...simulation, title: e.target.value });
-  };
+  const onChangeTitle = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (simulation == null) return;
+      setSimulation({ ...simulation, title: e.target.value });
+    },
+    [simulation]
+  );
 
-  const onChangePrincipal = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (simulation == null) return;
-    setSimulation({
-      ...simulation,
-      principal: Number(e.target.value)
-    });
-  };
+  const onChangePrincipal = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (simulation == null) return;
+      setSimulation({
+        ...simulation,
+        principal: Number(e.target.value)
+      });
+    },
+    [simulation]
+  );
 
-  const onChangeAssumedYieldsYear = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    order: number
-  ) => {
-    if (!Array.isArray(assumedYields)) return;
-    const newAssumedYields = assumedYields.map((assumedYield) => {
-      if (assumedYield.order === order) {
-        return { ...assumedYield, year: Number(e.target.value) };
-      }
-      return assumedYield;
-    });
-    setAssumedYields(newAssumedYields);
-  };
+  const onChangeAssumedYieldsYear = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>, order: number) => {
+      if (!Array.isArray(assumedYields)) return;
+      const newAssumedYields = assumedYields.map((assumedYield) => {
+        if (assumedYield.order === order) {
+          return { ...assumedYield, year: Number(e.target.value) };
+        }
+        return assumedYield;
+      });
+      setAssumedYields(newAssumedYields);
+    },
+    [assumedYields]
+  );
 
-  const onChangeAssumedYieldsRate = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    order: number
-  ) => {
-    if (!Array.isArray(assumedYields)) return;
-    const newAssumedYields = assumedYields.map((assumedYield) => {
-      if (assumedYield.order === order) {
-        return { ...assumedYield, rate: Number(e.target.value) };
-      }
-      return assumedYield;
-    });
-    setAssumedYields(newAssumedYields);
-  };
+  const onChangeAssumedYieldsRate = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>, order: number) => {
+      if (!Array.isArray(assumedYields)) return;
+      const newAssumedYields = assumedYields.map((assumedYield) => {
+        if (assumedYield.order === order) {
+          return { ...assumedYield, rate: Number(e.target.value) };
+        }
+        return assumedYield;
+      });
+      setAssumedYields(newAssumedYields);
+    },
+    [assumedYields]
+  );
 
-  const addAssumedYield = () => {
+  const addAssumedYield = useCallback(() => {
     setAssumedYields([
       ...assumedYields, // 既存の配列を展開
       {
@@ -115,41 +121,47 @@ export const Edit = () => {
         simulation_id: String(simulation_id)
       }
     ]);
-  };
+  }, [assumedYields, simulation_id]);
 
-  const deleteFrontAssumedYield = (order: number) => {
-    const newAssumedYields = assumedYields.filter((assumedYield) => {
-      console.log(assumedYield.order);
-      return assumedYield.order !== order;
-    });
-    // orderを振り直す
-    newAssumedYields.forEach((assumedYield, index) => {
-      assumedYield.order = index + 1;
-    });
-    setAssumedYields(newAssumedYields);
-  };
-
-  const saveData = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const newData: Simulation = {
-      title: simulation?.title || "",
-      principal: Number(simulation?.principal) || 0
-    };
-    // 一旦元のデータを消してから、新しいデータを入れる
-    // TODO: 消すのは成功したけど入れるの失敗したら詰むのなんとかする
-    await assumedYieldIdsRef.current.forEach((assumedYield) => {
-      deleteAssumedYield(assumedYield);
-    });
-    await assumedYields.forEach((assumedYield) => {
-      postAssumedYield({
-        order: assumedYield.order,
-        year: assumedYield.year,
-        rate: Number(assumedYield.rate),
-        simulation_id: String(simulation_id)
+  const deleteFrontAssumedYield = useCallback(
+    (order: number) => {
+      const newAssumedYields = assumedYields.filter((assumedYield) => {
+        console.log(assumedYield.order);
+        return assumedYield.order !== order;
       });
-    });
-    await updateSimulation(newData, String(simulation_id));
-  };
+      // orderを振り直す
+      newAssumedYields.forEach((assumedYield, index) => {
+        assumedYield.order = index + 1;
+      });
+      setAssumedYields(newAssumedYields);
+    },
+    [assumedYields]
+  );
+
+  const saveData = useCallback(
+    () => async (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      const newData: Simulation = {
+        title: simulation?.title || "",
+        principal: Number(simulation?.principal) || 0
+      };
+      // 一旦元のデータを消してから、新しいデータを入れる
+      // TODO: 消すのは成功したけど入れるの失敗したら詰むのなんとかする
+      await assumedYieldIdsRef.current.forEach((assumedYield) => {
+        deleteAssumedYield(assumedYield);
+      });
+      await assumedYields.forEach((assumedYield) => {
+        postAssumedYield({
+          order: assumedYield.order,
+          year: assumedYield.year,
+          rate: Number(assumedYield.rate),
+          simulation_id: String(simulation_id)
+        });
+      });
+      await updateSimulation(newData, String(simulation_id));
+    },
+    [simulation, assumedYields, simulation_id]
+  );
 
   return (
     <Flex direction="column" gap={2}>
