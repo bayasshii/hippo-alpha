4e1e5d2c031206a0f2b6bcb189d9b517f0bcb75d
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-11-05 21:01:54 +0900

    Update backend/db/seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -3,20 +3,19 @@ simulation1 = Simulation.create!(
     principal: 100000,
 )
 
-
 AssumedYield.create!(
-    
     simulation: simulation1,
     simulation_id: 1,
     rate: 3,
     order: 1,
     year: 10,
 )
+
 AssumedYield.create!(
     simulation: simulation1,
     simulation_id: 1,
     rate: 5,
-    order: 1,
+    order: 2,
     year: 20,
 )
 
@@ -27,17 +26,19 @@ MonthlyDeposit.create!(
     order: 1,
     year: 8,
 )
+
 MonthlyDeposit.create!(
     simulation: simulation1,
     simulation_id: 1,
     amount: 500,
-    order: 1,
+    order: 2,
     year: 3,
 )
+
 MonthlyDeposit.create!(
     simulation: simulation1,
     simulation_id: 1,
     amount: 5000,
-    order: 1,
+    order: 3,
     year: 19,
 )
