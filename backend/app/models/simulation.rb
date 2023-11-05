ced05bba9ea2d8cdc1cd00c5e425d27491bed99d
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-11-05 21:00:28 +0900

    サーバーバリデーション

--- backend/app/models/simulation.rb
+++ backend/app/models/simulation.rb
@@ -1,6 +1,6 @@
 class Simulation < ApplicationRecord
-    has_many :assumed_yields, dependent: :destroy
-    has_many :monthly_deposits, dependent: :destroy
-    validates :title,  presence: true, length: { maximum: 50 }
-    validates :principal,  presence: true, length: { maximum: 12 }
+  has_many :assumed_yields, dependent: :destroy
+  has_many :monthly_deposits, dependent: :destroy
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :principal, presence: true, length: { maximum: 12 }, numericality: { only_integer: true }
 end
