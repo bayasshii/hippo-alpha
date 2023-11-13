16f21f8a8f9daec384852fef6a21872b4281f12f
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-11-14 08:54:33 +0900

    cors設定変更

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins ENV['CORS_ALLOWED_ORIGINS'] || 'http://localhost:3000'
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
