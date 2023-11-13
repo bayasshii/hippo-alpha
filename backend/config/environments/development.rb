4643804d95a17e9d00ae4df2211eec5ebc5a6e82
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-11-13 22:21:57 +0900

    render, pscaleむけの設定

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -53,6 +53,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  
   config.action_mailer.default_options = { from: ENV['EMAIL_ADDRESS'] }
   config.action_mailer.default_url_options = { host: 'localhost:3001' }
   config.action_mailer.delivery_method = :smtp
