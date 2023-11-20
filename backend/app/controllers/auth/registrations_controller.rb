acc8cf18162579eba134d21298b178510a3a8b75
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-11-20 21:29:10 +0900

    Update registrations_controller.rb

--- backend/app/controllers/auth/registrations_controller.rb
+++ backend/app/controllers/auth/registrations_controller.rb
@@ -1,2 +1,8 @@
 class Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
+
+  def sign_up_params
+    # nameは一旦必要ぽい
+    params.require(:registration).permit(:email, :password, :password_confirmation, :name)
+  end
 end
