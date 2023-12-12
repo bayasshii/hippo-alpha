2241a09a116e30b17aeeb61dc28e9910d3f66293
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-12-12 09:50:55 +0900

    外部キー制約削除

--- backend/db/migrate/20231103050659_create_simulations.rb
+++ backend/db/migrate/20231103050659_create_simulations.rb
@@ -3,7 +3,7 @@ class CreateSimulations < ActiveRecord::Migration[7.0]
     create_table :simulations do |t|
       t.string "title", null: false
       t.string "principal", null: false
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false # planetscaleの仕様上外部キー制約は削除, foreign_key: true
       t.timestamps
     end
   end
--- backend/db/migrate/20231108082708_create_annual_simulations.rb
+++ backend/db/migrate/20231108082708_create_annual_simulations.rb
@@ -4,7 +4,7 @@ class CreateAnnualSimulations < ActiveRecord::Migration[7.0]
       t.integer "monthly_deposit", null: false
       t.integer "rate", null: false
       t.integer "year", null: false
-      t.references :simulation, null: false, foreign_key: true
+      t.references :simulation, null: false # planetscaleの仕様上外部キー制約は削除, foreign_key: true
       t.timestamps
     end
   end
