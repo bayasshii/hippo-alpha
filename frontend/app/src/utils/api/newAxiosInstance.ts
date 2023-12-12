bdf7edf1b83529b9d82b951eed6e9ce5bc86da17
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-12-12 09:56:17 +0900

    制限時間のバス

--- frontend/app/src/utils/api/newAxiosInstance.ts
+++ frontend/app/src/utils/api/newAxiosInstance.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 const DEFAULT_API_CONFIG = {
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 10000,
+  timeout: 100000,
   mode: "cors",
   credentials: "include", // クッキー使うならあった方がいい
   withCredentials: true,
@@ -13,8 +13,8 @@ const DEFAULT_API_CONFIG = {
     // ↓この辺はユーザーのログイン情報のロジックだし、ここで呼び出すのは適切じゃなさげな気がする
     "access-token": Cookies.get("_access_token"),
     client: Cookies.get("_client"),
-    uid: Cookies.get("_uid")
-  }
+    uid: Cookies.get("_uid"),
+  },
 };
 
 export const newAxiosInstance = () => {
