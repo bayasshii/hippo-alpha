80a7b306a1a7fc197abcfab03873166c82af130f
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-12-12 20:29:30 +0900

    ログイン時にcookieに情報を保存できるように

--- frontend/app/src/utils/api/newAxiosInstance.ts
+++ frontend/app/src/utils/api/newAxiosInstance.ts
@@ -1,25 +1,22 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const DEFAULT_API_CONFIG = {
-  baseURL: process.env.REACT_APP_API_URL,
-  timeout: 100000,
-  mode: "cors",
-  credentials: "include", // クッキー使うならあった方がいい
-  withCredentials: true,
-  headers: {
-    ContentType: "application/json",
-    Accept: "application/json",
-    // ↓この辺はユーザーのログイン情報のロジックだし、ここで呼び出すのは適切じゃなさげな気がする
-    "access-token": Cookies.get("_access_token"),
-    client: Cookies.get("_client"),
-    uid: Cookies.get("_uid"),
-  },
-};
-
 export const newAxiosInstance = () => {
-  const instance = axios.create(DEFAULT_API_CONFIG);
-
+  const axios_config = {
+    baseURL: process.env.REACT_APP_API_URL,
+    timeout: 100000,
+    mode: "cors",
+    credentials: "include", // クッキー使うならあった方がいい
+    withCredentials: true,
+    headers: {
+      ContentType: "application/json",
+      Accept: "application/json",
+      "access-token": Cookies.get("_access_token"),
+      client: Cookies.get("_client"),
+      uid: Cookies.get("_uid")
+    }
+  };
+  const instance = axios.create(axios_config);
   instance.interceptors.response.use(
     (response) => {
       return response;
