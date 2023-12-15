79e67a2b60f20ea685e359395f5f05a9687c1dc4
bayasshii <42494214+bayasshii@users.noreply.github.com>
2023-12-16 00:35:02 +0900

    サイドナビ

--- frontend/app/src/components/SideNavi/User.tsx
+++ frontend/app/src/components/SideNavi/User.tsx
@@ -1,12 +1,22 @@
 import { AuthContext } from "@/utils/auth/AuthProvider";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { FaceIcon } from "@/components/images/FaceIcon";
 
 export const User = () => {
   const { currentUser } = useContext(AuthContext);
   return (
-    <Link to="/user/edit" style={{ color: "#fff" }}>
-      {currentUser?.name}
+    <Link
+      to="/user/edit"
+      style={{
+        color: "#fff",
+        display: "flex",
+        gap: "0.5rem",
+        alignItems: "center"
+      }}
+    >
+      <FaceIcon />
+      <span>{currentUser?.name}</span>
     </Link>
   );
 };
